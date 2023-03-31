e681d3e0504b870870978e8051369e90f05f61c5
Thiago Assi <thiago-assi@outlook.com>
2023-03-31 20:52:51 -0300

    initial commit

---
+++ src/test/java/com/yellow/universe/projectgauss/ProjectGaussApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yellow.universe.projectgauss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGaussApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
