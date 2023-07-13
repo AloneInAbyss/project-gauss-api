16fa87708710fffa1c4b5d81a3c7f4fc4d5646ca
Thiago Assi <thiago-assi@outlook.com>
2023-07-13 19:28:37 -0300

    initial commit

---
+++ src/main/java/com/universe/yellow/project/gauss/api/ProjectGaussApiApplication.java
@@ -0,0 +1,13 @@
+package com.universe.yellow.project.gauss.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGaussApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGaussApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universe/yellow/project/gauss/api/ProjectGaussApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.universe.yellow.project.gauss.api;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGaussApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(1 < 2);
+	}
+
+}
